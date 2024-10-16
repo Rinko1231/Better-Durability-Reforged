ca0a999fdb93d8e5ee106df21b7bdb04ec0b124e
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2024-10-16 23:28:52 +0800

    Add files via upload

--- src/main/java/darkorg/betterdurability/BetterDurability.java
+++ src/main/java/darkorg/betterdurability/BetterDurability.java
@@ -2,12 +2,7 @@ package darkorg.betterdurability;
 
 import darkorg.betterdurability.setup.ConfigurationHandler;
 import darkorg.betterdurability.util.NaiveLoggerWrapper;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.ServerChatEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -24,17 +19,5 @@ public class BetterDurability {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @SubscribeEvent
-    public void onPlayerChat(ServerChatEvent event) {
-        Player player = event.getPlayer();
-        ItemStack heldItem = player.getMainHandItem();
-        if (!heldItem.isEmpty()) {
-            if (heldItem.isDamageableItem()) {
-                // 将物品的耐久值设为2（注意这是剩余耐久值，不是损耗的耐久）
-                heldItem.setDamageValue(heldItem.getMaxDamage() - 2);
-                player.displayClientMessage(Component.literal("Damaged"), true);
-            }
-        }
-    }
 
 }
